4069197586ecfd1d9a0ace854ef8af7b1120815b
Rhama Krisner <rhamakrisner@gmail.com>
2023-10-01 02:49:25 -0300

    add: Criado projeto

---
+++ src/main/java/app/impacto_manager/ImpactoManagerApplication.java
@@ -0,0 +1,13 @@
+package app.impacto_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactoManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImpactoManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/impacto_manager/ImpactoManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.impacto_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactoManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
