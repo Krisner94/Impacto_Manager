9b2ddda32c521cdb68a96ef4c1999e55188458dc
Rhama Krisner <rhamakrisner@gmail.com>
2023-12-21 14:22:25 -0300

    Adicionado tela de aluno e professor

---
+++ src/main/java/app/impacto_manager/controller/Aluno.java
@@ -0,0 +1,9 @@
+package app.impacto_manager.controller;
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@FxmlView("fxml/aluno.fxml")
+public class Aluno {
+}
